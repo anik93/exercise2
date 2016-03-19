a84081dfe3ce08d59b5210ee09f4386956202c59
anik93 <proxianik@gmail.com>
2016-03-19 11:11:11 +0100

    git push origin procon

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,21 @@
 package wdsr.exercise2.procon;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * Task: implement Exchange interface without using any *Queue classes from java.util.concurrent package.
  * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
-	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+	
+	Queue<Order> listOfOrder = new ArrayDeque<>(); 
+	
+	public synchronized void submitOrder(Order order) throws InterruptedException {
+		listOfOrder.add(order);
 	}
 	
-	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+	public synchronized Order consumeNextOrder() throws InterruptedException {
+		return listOfOrder.remove();
 	}
 }
