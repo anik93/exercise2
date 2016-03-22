f7e013f9a83b8b39d73a4e1b24b0b1bfca2deffa
Anik93 <proxianik@gmail.com>
2016-03-22 13:28:16 +0100

    1

--- src/main/java/wdsr/exercise2/startthread/BusinessServiceWithCallable.java
+++ src/main/java/wdsr/exercise2/startthread/BusinessServiceWithCallable.java
@@ -22,15 +22,12 @@ public class BusinessServiceWithCallable {
 	 * Each random number is calculated asynchronously.
 	 * @return sum of 100 random numbers.
 	 */
-	public long sumOfRandomInts() throws InterruptedException, ExecutionException {	
-		//long result = 0;
-		
+	public long sumOfRandomInts() throws InterruptedException, ExecutionException {			
 		// TODO Task: 
 		// 1. create 100 Callable objects that invoke helper.nextRandom in their call() method.
 		// 2. submit all Callable objects to executorService (executorService.submit or executorService.invokeAll)
 		// 3. sum up the results - each random number can be retrieved using future.get() method.
 		// 4. return the computed result.
-//		ExecutorService pool = Executors.newFixedThreadPool(100);
 
 		List<Callable<Integer>> tasks = new ArrayList<>();
 		for(int i = 0; i< 100; i++){
